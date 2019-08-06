c168b9c845c85dfef4b03e8274d06e3e28f3aca6
Ryan Ma <ryama@ucdavis.edu>
2019-08-05 22:14:18 -0700

    Working with an adapter and list view!

--- app/src/main/java/com/example/tutorialapplication/secondActivity.java
+++ app/src/main/java/com/example/tutorialapplication/secondActivity.java
@@ -1,20 +1,29 @@
 package com.example.tutorialapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class secondActivity extends AppCompatActivity {
+
+    ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        String[] items;
+        items = res.getStringArray(R.array.items);
 
-        if(getIntent().hasExtra("com.example.tutorialapplication.SOMETHING")){
-            TextView tv = (TextView) findViewById(R.id.groceryListTextView);
-            String text = getIntent().getExtras().getString("com.example.tutorialapplication.SOMETHING");
-            tv.setText(text);
-        }
+
+//        if(getIntent().hasExtra("com.example.tutorialapplication.SOMETHING")){
+//            TextView tv = (TextView) findViewById(R.id.groceryListTextView);
+//            String text = getIntent().getExtras().getString("com.example.tutorialapplication.SOMETHING");
+//            tv.setText(text);
+//        }
+
     }
 }
